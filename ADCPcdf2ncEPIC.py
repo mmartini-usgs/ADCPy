8122cd96d921d7f58dd8cc90b5e3675ec72511de
mmartini-usgs <mmartini@usgs.gov>
2017-07-13 14:40:38 -0400

    changed rotation methods to that from dolfyn.  Got close, need to fine tune how roll is handled for up/down looking instruments and test

--- ADCPcdf2ncEPIC.py
+++ ADCPcdf2ncEPIC.py
@@ -203,10 +203,73 @@ def doEPIC_ADCPfile(cdfFile, ncFile, attFile, settings):
     
     print('converting %d ensembles from beam to earth %s' % (len(nc['time']), dt.datetime.now()))
     
-    if rawcdf.TRDI_Beam_Pattern == "Convex":
-        convex = True
-    else:
-        convex = False
+    # check our indexing
+    print('heading[0] = %f' % heading[0])
+    print('rawcdf Hdg[s] = %f for s = %d' % (rawcdf['Hdg'][s],s))
+    print('nc Hdg_1215[0] = %f' % nc['Hdg_1215'][0,0,0])
+    print('declination = %f' % declination)
+    # TODO replace this with xarray methodology when we understand how to write
+    # xarray to netcdf
+    
+    # TODO add depth bin scaling
+
+    # this beam arrangement is for TRDI Workhorse and V
+    rawvarnames = ["vel1", "vel2", "vel3", "vel4"]
+    ncidx = 0
+    if settings['transformation'].upper() == "BEAM":
+        ncvarnames = ["Beam1", "Beam2", "Beam3", "Beam4"]        
+        for idx in range(s,e):
+            for beam in range(nbeams):
+                nc[ncvarnames[beam]][ncidx,:,0,0] = \
+                    rawcdf.variables[rawvarnames[beam]][idx,:] /10
+            ncidx = ncidx + 1
+            
+    elif (settings['transformation'].upper() == "INST") or \
+        (settings['transformation'].upper() == "EARTH"):
+        ncvarnames = ["X","Y","Z","Error"]            
+        # the dolfyn way
+        # load the ADCP data object - we have converted this from a class object
+        # to nested dictionaries for use here
+        adcpo = {}
+        adcpo['props'] = {}
+        adcpo['props']['coord_sys'] = "beam"
+        adcpo['props']['inst2earth:fixed'] = False
+        adcpo['config'] = {}
+        adcpo['config']['beam_angle'] = nc.beam_angle
+        adcpo['config']['beam_pattern'] = nc.beam_pattern
+        adcpo['config']['orientation'] = nc.orientation
+        adcpo['declination_in_heading'] = True
+        # dolfyn shape for ensemble data is [bins x beams x ens]
+        #adcpo['vel'] = np.ones([nbins,nbeams,1],dtype='float')*np.NAN
+        adcpo['vel'] = np.ones([nbins,nbeams],dtype='float')*np.NAN
+        # TODO because I'm not good at np array manipulation yet - improve this
+        # for now, vels has to be pre-defined to get the shapes to broadcast
+        vels = np.ones([nbins,1],dtype='float')*np.NAN
+        
+        for idx in range(s,e):
+            for beam in range(nbeams):
+                # load data of one ensemble to dolfyn shape, in cm/s
+                #adcpo['vel'][:,beam,0] = rawcdf.variables[rawvarnames[beam]][idx,:] * 0.1
+                vels = rawcdf.variables[rawvarnames[beam]][idx,:] * 0.1
+                adcpo['vel'][:,beam] = vels
+            
+            # need to keep setting this with new beam data since we are iterating
+            adcpo['props']['coord_sys'] = "beam" 
+            beam2inst(adcpo) # adcpo['vel'] is returned in inst coordinates
+        
+            if settings['transformation'].upper() == "EARTH":
+                ncvarnames = ["u_1205","v_1206","w_1204","Werr_1201"]
+                adcpo['heading_deg'] = nc.variables['Hdg_1215'][ncidx]
+                adcpo['pitch_deg'] = nc.variables['Ptch_1216'][ncidx]
+                adcpo['roll_deg']  = nc.variables['Roll_1217'][ncidx]
+                inst2earth(adcpo)
+                
+            for beam in range(nbeams):
+                nc[ncvarnames[beam]][ncidx,:,0,0] = adcpo['vel'][:,beam] 
+                   
+            ncidx = ncidx + 1
+    
+    """    
     
     # set up containers to operate on
     vbeam =  np.asmatrix(np.ones([nbeams,nbins], dtype=float, order='C')*np.NAN)
@@ -214,13 +277,7 @@ def doEPIC_ADCPfile(cdfFile, ncFile, attFile, settings):
     #vxyz =  np.asmatrix(np.ones([3,nbins], dtype=float, order='C')*np.NAN)
     #vearth =  np.asmatrix(np.ones([nbeams,nbins], dtype=float, order='C')*np.NAN)
     vels = np.ones(nbins,dtype=float, order='C')*np.NAN
-    
-    
-    # TODO replace this with xarray methodology when we understand how to write
-    # xarray to netcdf
-    
-    # TODO add depth bin scaling
-        
+            
     # this beam arrangement is for TRDI Workhorse and V
     rawvarnames = ["vel1", "vel2", "vel3", "vel4"]
     ncidx = 0
@@ -270,6 +327,7 @@ def doEPIC_ADCPfile(cdfFile, ncFile, attFile, settings):
                     vels = np.array(vinst[beam,:])
                 nc[ncvarnames[beam]][ncidx,:,0,0] = vels                    
             ncidx = ncidx +1
+    """
             
     # TODO minima and maxima here - or as a separate operation using xarray
     # in fact - using xarray will allow practice in xarray operations and 
@@ -278,10 +336,9 @@ def doEPIC_ADCPfile(cdfFile, ncFile, attFile, settings):
     print('closing files at %s' % (dt.datetime.now()))
 
     rawcdf.close()
-    nc.close()
-   
-    return
+    nc.close()            
 
+   
 """ this transformation matrix is from the R.D. Instruments Coordinate 
     Transformation booklet.  It presumes the beams are in the same position as
     RDI Workhorse ADCP beams, where, when looking down on the transducers:
@@ -299,33 +356,6 @@ def doEPIC_ADCPfile(cdfFile, ncFile, attFile, settings):
         TRDI beam 2 = Nortek beam 4
         Heading, pitch and roll behave the same as TRDI
 """
-# this code thanks to https://github.com/lkilcher/dolfyn  rotate.py
-def calc_beam_rotmatrix(theta=20, convex=True, degrees=True):
-    """Calculate the rotation matrix from beam coordinates to
-    instrument head coordinates.
-    Parameters
-    ----------
-    theta : is the angle of the heads (usually 20 or 30 degrees)
-    convex : is a flag for convex or concave head configuration.
-    degrees : is a flag which specifies whether theta is in degrees
-        or radians (default: degrees=True)
-    """
-    deg2rad = np.pi / 180.
-    
-    if degrees:
-        theta = theta * deg2rad
-    if convex == 0 or convex == -1:
-        c = -1
-    else:
-        c = 1
-    a = 1 / (2. * np.sin(theta))
-    b = 1 / (4. * np.cos(theta))
-    d = a / (2. ** 0.5)
-    return np.asmatrix(np.array([[c * a, -c * a, 0, 0],
-                     [0, 0, -c * a, c * a],
-                     [b, b, b, b],
-                     [d, d, -d, -d]]))
-
 # as per the R.D. Instruments Coordinate Transformation booklet                     
 def cal_earth_rotmatrix(heading=0,pitch=0,roll=0,declination=0):
     # for declination, West is negative
@@ -340,6 +370,176 @@ def cal_earth_rotmatrix(heading=0,pitch=0,roll=0,declination=0):
     return np.asmatrix(np.array([[(ch*cr+sh*sp*sr), (sh*cp), (ch*sr-sh*sp*cr)],
                                 [(-sh*cr+ch*sp*sr), (ch*cp), (-sh*sr-ch*sp*cr)],
                                 [(-cp*sr), sp, (cp*cr)]]))
+
+# as per dolfyn rotate.py code here: https://github.com/lkilcher/dolfyn
+
+def calc_beam_rotmatrix(theta=20, convex=True, degrees=True):
+    """Calculate the rotation matrix from beam coordinates to
+    instrument head coordinates.
+
+    Parameters
+    ----------
+    theta : is the angle of the heads (usually 20 or 30 degrees)
+
+    convex : is a flag for convex or concave head configuration.
+
+    degrees : is a flag which specifies whether theta is in degrees
+        or radians (default: degrees=True)
+    """
+    deg2rad = np.pi / 180.
+    if degrees:
+        theta = theta * deg2rad
+    if convex == 0 or convex == -1:
+        c = -1
+    else:
+        c = 1
+    a = 1 / (2. * np.sin(theta))
+    b = 1 / (4. * np.cos(theta))
+    d = a / (2. ** 0.5)
+    return np.array([[c * a, -c * a, 0, 0],
+                     [0, 0, -c * a, c * a],
+                     [b, b, b, b],
+                     [d, d, -d, -d]])
+
+
+def _cat4rot(tpl):
+    tmp = []
+    for vl in tpl:
+        tmp.append(vl[:, None, :])
+    return np.concatenate(tuple(tmp), axis=1)
+
+def beam2inst(adcpo, reverse=False, force=False):
+    """Rotate velocitiesfrom beam to instrument coordinates.
+
+    Parameters
+    ----------
+    adpo : The ADP object containing the data.
+
+    reverse : bool (default: False)
+           If True, this function performs the inverse rotation
+           (inst->beam).
+    force : bool (default: False)
+        When true do not check which coordinate system the data is in
+        prior to performing this rotation.
+    """
+    if not force:
+        if not reverse and adcpo['props']['coord_sys'] != 'beam':
+            raise ValueError('The input must be in beam coordinates.')
+        if reverse and adcpo['props']['coord_sys'] != 'inst':
+            raise ValueError('The input must be in inst coordinates.')
+    if 'rotmat' in adcpo['config'].keys():  
+        rotmat = adcpo['config']['rotmat']
+    else:
+        rotmat = calc_beam_rotmatrix(adcpo['config']['beam_angle'],
+                                     adcpo['config']['beam_pattern'].lower()
+                                     == 'convex')
+    cs = 'inst'
+    if reverse:
+        # Can't use transpose because rotation is not between
+        # orthogonal coordinate systems
+        rotmat = np.linalg.inv(rotmat)
+        cs = 'beam'
+    #raw = adcpo['vel'].transpose()
+    raw = np.asmatrix(adcpo['vel'])
+    # here I end up with an extra dimension of 4
+    #vels = np.einsum('ij,jkl->ikl', rotmat, raw)
+    #vels = np.einsum('ij,jk->ik', rotmat, raw)
+    vels = np.array(np.asmatrix(rotmat)*raw.transpose())
+    #vels = np.einsum('ij,jkl->ikl', rotmat, adcpo['vel'])
+    #ValueError: operands could not be broadcast together with remapped 
+    # shapes [original->remapped]: (4,4)->(4,newaxis,newaxis,4) (16,4,1)->(4,1,16) 
+    #adcpo['vel'] = np.einsum('ij,jkl->ikl', rotmat, adcpo['vel'])
+    adcpo['vel'] = vels.transpose()
+    adcpo['props']['coord_sys'] = cs
+
+def inst2earth(adcpo, reverse=False, fixed_orientation=False, force=False):
+    """Rotate velocities from the instrument to earth coordinates.
+
+    Parameters
+    ----------
+    adpo : The ADP object containing the data.
+
+    reverse : bool (default: False)
+           If True, this function performs the inverse rotation
+           (earth->inst).
+    fixed_orientation : bool (default: False)
+        When true, take the average orientation and apply it over the
+        whole record.
+    force : bool (default: False)
+        When true do not check which coordinate system the data is in
+        prior to performing this rotation.
+
+    Notes
+    -----
+    The rotation matrix is taken from the Teledyne RDI ADCP Coordinate
+    Transformation manual January 2008
+
+    When performing the forward rotation, this function sets the
+    'inst2earth:fixed' flag to the value of `fixed_orientation`. When
+    performing the reverse rotation, that value is 'popped' from the
+    props dict and the input value to this function
+    `fixed_orientation` has no effect. If `'inst2earth:fixed'` is not
+    in the props dict than the input value *is* used.
+    """
+    deg2rad = np.pi / 180.
+    if not force:
+        if not reverse and adcpo['props']['coord_sys'] != 'inst':
+            raise ValueError('The input must be in inst coordinates.')
+        if reverse and adcpo['props']['coord_sys'] != 'earth':
+            raise ValueError('The input must be in earth coordinates.')
+    if (not reverse and 'declination' in adcpo['props'].keys() and not
+            adcpo['props']['declination_in_heading']):
+        # Only do this if making the forward rotation.
+        adcpo['heading_deg'] += adcpo['props']['declination']
+        adcpo['props']['declination_in_heading'] = True
+    r = adcpo['roll_deg'] * deg2rad
+    p = np.arctan(np.tan(adcpo['pitch_deg'] * deg2rad) * np.cos(r))
+    h = adcpo['heading_deg'] * deg2rad
+    if adcpo['config']['orientation'] == 'up': 
+        r += np.pi
+    ch = np.cos(h)
+    sh = np.sin(h)
+    cr = np.cos(r)
+    sr = np.sin(r)
+    cp = np.cos(p)
+    sp = np.sin(p)
+    rotmat = np.empty((3, 3, len(r)))
+    rotmat[0, 0, :] = ch * cr + sh * sp * sr
+    rotmat[0, 1, :] = sh * cp
+    rotmat[0, 2, :] = ch * sr - sh * sp * cr
+    rotmat[1, 0, :] = -sh * cr + ch * sp * sr
+    rotmat[1, 1, :] = ch * cp
+    rotmat[1, 2, :] = -sh * sr - ch * sp * cr
+    rotmat[2, 0, :] = -cp * sr
+    rotmat[2, 1, :] = sp
+    rotmat[2, 2, :] = cp * cr
+    # Only operate on the first 3-components, b/c the 4th is err_vel
+    ess = 'ijk,jlk->ilk'
+    cs = 'earth'
+    if reverse:
+        cs = 'inst'
+        fixed_orientation = adcpo['props'].pop('inst2earth:fixed', fixed_orientation)
+        ess = ess.replace('ij', 'ji')
+    else:
+        adcpo['props']['inst2earth:fixed'] = fixed_orientation
+    if fixed_orientation:
+        ess = ess.replace('k,', ',')
+        rotmat = rotmat.mean(-1)
+    #vels = np.einsum(ess, rotmat, adcpo['vel'][:,:3])
+    vels = np.asmatrix(rotmat) * np.asmatrix(adcpo['vel'][:,:3].transpose())
+    adcpo['vel'][:,:3] = vels.transpose()
+    adcpo['props']['coord_sys'] = cs
+
+def inst2earth_heading(adpo):
+    deg2rad = np.pi / 180.
+    h = adpo.heading_deg[:] * deg2rad
+    if 'heading_offset' in adpo.props.keys():
+        h += adpo['props']['heading_offset'] * deg2rad
+    if 'declination' in adpo['props'].keys():
+        h += adpo['props']['declination'] * deg2rad
+    return np.exp(-1j * h)
+    
+# end code from dolfyn
 
 def setupEPICnc(fname, rawcdf, attfile, settings):
      
@@ -421,6 +621,9 @@ def setupEPICnc(fname, rawcdf, attfile, settings):
         cdf.beam_angle = rawcdf.TRDI_Beam_Angle
         cdf.number_of_slant_beams = rawcdf.TRDI_Number_of_Beams
         cdf.heading_bias_applied_EB = rawcdf.TRDI_Heading_Bias_Hundredths_of_Deg
+        cdf.beam_angle = rawcdf.TRDI_Beam_Angle
+        cdf.beam_pattern = rawcdf.TRDI_Beam_Pattern
+
     
     # attributes requiring user input
     cdf.transducer_offset_from_bottom = settings['transducer_offset_from_bottom']
@@ -794,6 +997,7 @@ def __main():
     doEPIC_ADCPfile(infileName, outfileName, attfileName, settings)
     
     print('Finished file conversion at ',dt.datetime.now())
+    
 
 
 if __name__ == "__main__":
