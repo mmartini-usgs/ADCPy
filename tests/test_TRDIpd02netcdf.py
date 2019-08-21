7a978314759bdea804e65078a01e13ba67ffb5e6
Marinna Martini <mmartini@usgs.gov>
2019-08-21 16:18:41 -0400

    update assert for new tiny file

--- tests/test_TRDIpd02netcdf.py
+++ tests/test_TRDIpd02netcdf.py
@@ -13,11 +13,13 @@ def trdipd0_convert_pd0file():
     import numpy as np
     # this is a test data file, very simple ADCP file
     # of older Workhorse ADCP data with only profiler
+    # you will use assert ensemble_count == 499 for this file
     # pd0_input_file = here.joinpath("9991wh000.000")
     # netcdf_output = here.joinpath("9991wh000.cdf")
     # serial_number = "473"
     # delta_t = "900"  # seconds, as a string
     # this is a test data file of a larger data set of Sentinel V data
+    # you will use assert ensemble_count == 8430 for this file
     pd0_input_file = here.joinpath("11121whV23857profiles.pd0")
     serial_number = "23857"
     delta_t = "3600"  # seconds, as a string
@@ -33,6 +35,6 @@ def trdipd0_convert_pd0file():
 
 def test__output(trdipd0_convert_pd0file):
     ensemble_count, netcdf_index, read_error = trdipd0_convert_pd0file
-    assert ensemble_count == 499
+    assert ensemble_count == 8430
     assert read_error is None
 
