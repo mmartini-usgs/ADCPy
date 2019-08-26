6285574ec3963c4e8bc24e0fa18b1b47ed6ba234
Marinna Martini <mmartini@usgs.gov>
2019-08-26 15:36:52 -0400

    remove top __init__

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from adcpy.TRDIstuff import TRDIpd0tonetcdf
-from adcpy.Nortekstuff import Norteknc2USGScdf
-from adcpy.EPICstuff import EPICmisc
-
-__all__ = ["ADCPcdf2ncEPIC", "EPICmisc", "repopulateEPIC", "reshapeEPIC", "Norteknc2USGScdf", "TRDIpd0tonetcdf"]
